c7d0c7c433304e9013b66de3bd20abe32b99a326
Серж Сидельников <serj@MacBook-Pro-Serz.local>
2020-02-29 19:57:11 +0300

    Markup layouts and pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'semi': [2, 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
