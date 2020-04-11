7da03560f6823b9b602d0a6fefaaa76f7cebd9ec
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2020-04-11 17:26:28 +0300

    Planning page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Loader from './components/app/Loader';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import tooltipDirective from './directives/tooltip.directive';
 import dateFilter from './filters/date.filters';
 import currencyFilter from './filters/currency.filter';
 import messagePlugin from './utils/message.plugin';
@@ -20,6 +21,7 @@ Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 Vue.filter('currency', currencyFilter);
+Vue.directive('tooltip', tooltipDirective);
 Vue.component('Loader', Loader);
 
 firebase.initializeApp({
