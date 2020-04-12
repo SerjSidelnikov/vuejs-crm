47eeabd59cfa5cebb83e1a5643b9f7fcc24bf040
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2020-04-12 15:19:52 +0300

    Chartjs vue

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading"/>
@@ -29,12 +29,13 @@
 </template>
 
 <script>
+import { Pie } from 'vue-chartjs';
 import HistoryTable from '../components/HistoryTable';
 import paginationMixin from '../mixins/pagination.mixin';
 
 export default {
   name: 'history',
-
+  extends: Pie,
   mixins: [paginationMixin],
 
   data: () => ({
@@ -54,7 +55,48 @@ export default {
       };
     }));
 
+    this.setup(categories);
+
     this.loading = false;
+  },
+
+  methods: {
+    setup (categories) {
+      this.renderChart(
+        {
+          labels: categories.map((c) => c.title),
+          datasets: [{
+            label: 'Расходы по категориям',
+            data: categories.map((c) => {
+              return this.records.reduce((acc, r) => {
+                if (r.categoryId === c.id && r.type === 'outcome') {
+                  acc += +r.amount;
+                }
+
+                return acc;
+              }, 0);
+            }),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        }
+      );
+    }
   },
 
   components: {
