aad4001063e2bf91bace33a6da7bfbc6683da89b
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2020-04-12 15:01:26 +0300

    Pagination

---
+++ src/mixins/pagination.mixin.js
@@ -0,0 +1,27 @@
+import chunk from 'lodash/chunk';
+import size from 'lodash/size';
+
+export default {
+  data () {
+    return {
+      page: +this.$route.query.page || 1,
+      pageSize: 5,
+      pageCount: 0,
+      allItems: [],
+      items: []
+    };
+  },
+
+  methods: {
+    setupPagination (allItems) {
+      this.allItems = chunk(allItems, this.pageSize);
+      this.pageCount = size(this.allItems);
+      this.items = this.allItems[this.page - 1] || this.allItems[0];
+    },
+
+    handleChange (page) {
+      this.$router.push(`${this.$route.path}?page=${page}`);
+      this.items = this.allItems[page - 1] || this.allItems[0];
+    }
+  }
+};
